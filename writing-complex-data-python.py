b9d0e1fc9be631c5da54cf54bfa9fd4971d32dd7
Gurjas Singh <gurjassingh92@gmail.com>
2018-06-02 17:00:04 +0530

    Create writing-complex-data-python.py

---
+++ writing-complex-data-python.py
@@ -0,0 +1,53 @@
+import pyrebase
+
+# configuring your firebase database
+config = {
+  
+  # credentials taken from Firebase 'Web-Setup'
+  
+  "apiKey": "AIzaSyD2UCfuM-iFiMBKBT9uHfeSuNct5b-a4Wc",
+  "authDomain": "city-tour-guide-5f8f0.firebaseapp.com",
+  "databaseURL": "https://city-tour-guide-5f8f0.firebaseio.com",
+  "storageBucket": "city-tour-guide-5f8f0.appspot.com"
+}
+
+# initializing the firebase object
+firebase = pyrebase.initialize_app(config)
+
+# providing the login details for the Authentication
+# we need to provide login details because as per the rules of firebase, we cannot write anything until
+# unless the user is not authorized user.
+
+email = 'brillicaservice@gmail.com'
+password = "Brillica@92G!"
+
+# initializing an auth variable that will authorize the app
+auth = firebase.auth()
+
+# try catch functionality in case the login details are incorrect.
+try:
+
+    # use this function to sign in with email and password
+    user = auth.sign_in_with_email_and_password(email, password)
+    
+    # after authorization, initializing the firebase databse.
+    fb = firebase.database()
+    
+    # writing the data into a JSON structure i.e. key-value pair
+    data = {
+        "name": "Rohit",
+        "course": "Python",
+        "fees-paid": "True"
+    }
+
+    # telling firebase where to put this data
+    # as of now, we are inserting this data into 'Students-List' node and 
+    # sending the authenticated user token as well while pushing the new data
+    fb.child("students-list").push(data, user['idToken'])
+    
+    # print succcess message once the data has been pushed successfully
+    print('New value inserted successfully')
+    
+# incase there is any exception that has raised    
+except Exception:
+    print('Sorry some error occurred. Please try again later.')
