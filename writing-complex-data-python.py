f87ade48fc06428e5f7a69f83f8f98776fcbc050
Gurjas Singh <gurjassingh92@gmail.com>
2018-06-03 09:08:29 +0530

    Update writing-complex-data-python.py

--- writing-complex-data-python.py
+++ writing-complex-data-python.py
@@ -5,10 +5,10 @@ config = {
   
   # credentials taken from Firebase 'Web-Setup'
   
-  "apiKey": "AIzaSyD2UCfuM-iFiMBKBT9uHfeSuNct5b-a4Wc",
-  "authDomain": "city-tour-guide-5f8f0.firebaseapp.com",
-  "databaseURL": "https://city-tour-guide-5f8f0.firebaseio.com",
-  "storageBucket": "city-tour-guide-5f8f0.appspot.com"
+  "apiKey": "************************c",
+  "authDomain": "************************.firebaseapp.com",
+  "databaseURL": "************************.firebaseio.com",
+  "storageBucket": "************************.appspot.com"
 }
 
 # initializing the firebase object
